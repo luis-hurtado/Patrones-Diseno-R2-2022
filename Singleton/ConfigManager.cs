c23ea97a7c63b2a7f676a33813ff7d99533b21d5
Luis Hurtado <lhurtado@visionweb.com>
2022-12-01 19:29:58 -0400

    Singleton

---
+++ Singleton/ConfigManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Singleton
+{
+    internal class ConfigManager
+    {
+        private IDictionary<string, object> _settings = new Dictionary<string, object>();
+
+        private static ConfigManager _instance;
+
+        private ConfigManager() {}
+
+        public void Set(string llave, object valor)
+        {
+            _settings[llave] = valor;
+        }
+
+        public object Get(string llave)
+        {
+            object valor = null;
+            if (_settings.ContainsKey(llave))
+            {
+                valor = _settings[llave];
+            }
+            return valor;
+        }
+
+        public static ConfigManager GetInstance()
+        {
+            return _instance ?? (_instance = new ConfigManager());
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigManager manejador = ConfigManager.GetInstance();
+            manejador.Set("Nombre_Aplicacion", "MiAplicacion");
+            manejador.Set("Version", 1);
+            Console.WriteLine($"El valor de setting Nombre_Aplicacion es: {manejador.Get("Nombre_Aplicacion")}");
+
+            ConfigManager otroManejador = ConfigManager.GetInstance();
+            Console.WriteLine($"El valor de setting Nombre_Aplicacion es: {otroManejador.Get("Nombre_Aplicacion")}");
+
+            if (manejador == otroManejador)
+                Console.WriteLine("Son iguales");
+            else
+                Console.WriteLine("Son diferentes");
+
+            Console.ReadLine();
+        }
+    }
+}
